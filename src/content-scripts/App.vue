d4a5732c18adaba0ddb2418f920479340465d83a
thatguychrisw <thatguychrisw@gmail.com>
2020-09-26 13:38:01 -0400

    Convert app to composition api style

--- src/content-scripts/App.vue
+++ src/content-scripts/App.vue
@@ -4,25 +4,29 @@
 
 <script>
 import CommandBar from '@/components/CommandBar'
+import { ref, onMounted } from 'vue'
 
 export default {
   name: 'App',
   components: {CommandBar},
-  data () {
-    return {
-      show: false,
-    }
-  },
-  created () {
-    window.addEventListener('keydown', (e) => {
-      if (e.metaKey && e.ctrlKey && e.key === 'p') {
-        this.show = true
-      }
+  setup () {
+    const show = ref(false)
 
-      if (e.key === "Escape") {
-        this.show = false
-      }
-    });
+    onMounted(() => {
+      window.addEventListener('keydown', (e) => {
+        if (e.metaKey && e.ctrlKey && e.key === 'p') {
+          show.value = true
+        }
+
+        if (e.key === 'Escape') {
+          show.value = false
+        }
+      })
+    })
+
+    return {
+      show
+    }
   }
 }
 </script>
