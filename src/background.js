302af0443148d12de5a22c9e5f73fd04972904d1
thatguychrisw <thatguychrisw@gmail.com>
2020-10-07 00:19:22 -0400

    Create background alarm foundation

--- src/background.js
+++ src/background.js
@@ -1,5 +1,27 @@
-browser.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.sendMessage(tab.id, { content: 'message' }, function(response) {
-    console.log(response)
-  })
+import ky from 'ky'
+
+const prettifyJson = obj => JSON.stringify(obj, null, 2)
+
+// schedules
+chrome.alarms.create('cache-shortcuts', { delayInMinutes: 1, periodInMinutes: 1 })
+
+// listeners
+chrome.alarms.onAlarm.addListener(async alarm => {
+  await cacheShortcutManifest(alarm)
 })
+
+const cacheShortcutManifest = async alarm => {
+  if (alarm.name !== 'cache-shortcuts') return
+
+  console.log('caching shortcut manifest')
+
+  const manifest = await ky
+    .get('https://chrome-command-bar.netlify.app/shortcut-manifest.json')
+    .json()
+
+  localStorage.setItem('ccb-shortcut-manifest', prettifyJson(manifest))
+
+  console.log('stored shortcut manifest in local storage')
+
+  return manifest
+}
