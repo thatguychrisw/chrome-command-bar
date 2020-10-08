cd15040cf8a2ea93c0b72b0b8a69ea546c647c17
Daniel Perez <perez.daniel2105@gmail.com>
2020-10-08 16:47:07 -0400

    Use renderless menu component

--- src/components/CommandBar.vue
+++ src/components/CommandBar.vue
@@ -1,24 +1,58 @@
 <template>
   <div v-if="show" class="chrome-cmd-bar-container">
     <div class="chrome-cmd-bar-card">
-      <search-input v-model="term" />
-      <nav-tips />
-      <search-results :results="results" @select="fireEvent" />
+      <Listbox v-model="selectedResult">
+        <ListboxButton v-model="term" :as="SearchInput" />
+        <nav-tips />
+        <transition
+          leave-to-class="_opacity-0"
+          leave-from-class="_opacity-100"
+          leave-active-class="_transition _ease-in _duration-100"
+        >
+          <div class="_z-20 _relative _grid _gap-6 _bg-white _px-2 _py-6" v-if="results.length">
+            <ListboxOptions static>
+              <!-- Disabled options will be skipped by keyboard navigation. -->
+              <ListboxOption
+                :key="i"
+                :value="result"
+                v-slot="{ active }"
+                v-for="(result, i) in results"
+              >
+                <search-result :result="result" :active="active" />
+              </ListboxOption>
+            </ListboxOptions>
+          </div>
+        </transition>
+      </Listbox>
+
+      <div>{{ term }} {{ selectedResult }}</div>
     </div>
   </div>
 </template>
 
 <script>
+import { Listbox, ListboxOptions, ListboxButton, ListboxOption } from '@headlessui/vue'
+
 import NavTips from './NavTips'
 import SearchInput from './SearchInput'
-import SearchResults from './SearchResults'
+import SearchResult from './SearchResult'
 
 export default {
   name: 'CommandBar',
-  components: { NavTips, SearchInput, SearchResults },
+  components: {
+    NavTips,
+    Listbox,
+    SearchResult,
+    ListboxOption,
+    ListboxButton,
+    ListboxOptions
+  },
   props: ['show'],
   data: () => ({
+    SearchInput,
+
     term: '',
+    selectedResult: {},
     results: [
       {
         label: 'Create New Issue',
